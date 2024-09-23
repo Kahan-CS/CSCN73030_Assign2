dc6d2ecfef450b6231d73aa81566b2d0b0220937
Kahan Desai <kdesai6826@conestogac.on.ca>
2024-09-22 23:17:42 -0400

    added initial source code

--- Source.cpp
+++ Source.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
-int main()
+struct STUDENT_DATA
 {
-	cout << "Hello, World!" << endl;
+	string fname;
+	string lname;
+};
+
+int main() {
+	ifstream fin("StudentData.txt");
+	vector<STUDENT_DATA> students;
+	string fname, lname;
+
+	if (fin.is_open()) {
+		cout << "Operation successfully performed\n";
+
+		while (getline(fin, fname, ',') && getline(fin, lname)) { // Directly split the line
+			students.push_back({ fname, lname }); // Add to vector in one step
+		}
+
+		fin.close();
+
+		/*for (const auto& student : students) {
+			cout << "First Name: " << student.fname << ", Last Name: " << student.lname << endl;
+		}*/
+	}
+	else {
+		cout << "Error opening file";
+	}
+
 	return 0;
 }
